dc7e6382479200d239efb302db138b936bfd2411
Jorge Ruiz <jorgeruiz@MacBook-Air-de-Jorge.local>
2022-11-11 09:47:04 -0600

    Tratando de recibir el enlace de node

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:paypal_flutter/manager.dart';
+import 'SuccessPage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,11 +13,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Paypal Test'),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const MyHomePage(title: "Flutter PayPal test"),
+        '/success': (context) => SuccessPage(),
+      },
     );
   }
 }
